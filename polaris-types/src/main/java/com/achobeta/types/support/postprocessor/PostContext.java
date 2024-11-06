aa55a48d76c6add603249bf88c6fe328af86016e
BanTanger <88583317+BanTanger@users.noreply.github.com>
2024-11-07 00:12:10 +0800

    feat: 增添与调整相关工具包 (#6)
    1. 彻底移除 design-rule-demo 相关依赖
    2. 新增 Redis、Redisson 配置
    3. PostContext 新增 extra 属性可供随意扩展

--- polaris-types/src/main/java/com/achobeta/types/support/postprocessor/PostContext.java
+++ polaris-types/src/main/java/com/achobeta/types/support/postprocessor/PostContext.java
@@ -1,6 +1,9 @@
 package com.achobeta.types.support.postprocessor;
 
+import com.achobeta.types.common.Constants;
 import lombok.*;
+
+import java.util.Map;
 
 /**
  * @author chensongmin
@@ -18,6 +21,7 @@ public class PostContext<T> {
 
     /**
      * 业务名称
+     * <p>新开一个业务模块，就需要在 {@link Constants.BizModule} 多开一个枚举映射上</p>
      */
     private String bizName;
 
@@ -25,5 +29,16 @@ public class PostContext<T> {
      * 业务数据
      */
     private T bizData;
+
+    /**
+     * 额外业务数据
+     * <p>如果在 processor 处理过程中出现了一些新数据需要承载
+     * 而不想修改原本定义好的 BO 对象时，可以往 extra 里放元素
+     * </p>
+     * <pre>数据格式：
+     * Key：变量名，小写开头，可以用常量封装
+     * Value：变量值</pre>
+     */
+    private Map<String, Object> extra;
 
 }
