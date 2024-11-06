ec99da83d034d62f015644f79242f3744a37791e
Aseubel <1228392319@qq.com>
2024-11-07 01:27:59 +0800

    feat: 添加user的部分实体类

---
+++ polaris-domain/src/main/java/com/achobeta/domain/user/model/entity/UserEntity.java
@@ -0,0 +1,68 @@
+package com.achobeta.domain.user.model.entity;
+
+import lombok.*;
+
+/**
+ * @author yangzhiyao
+ * @description 用户实体对象
+ * @date 2024/11/5
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+    /**
+     * 用户业务id
+     */
+    private String userId;
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 用户手机号
+     */
+    private String phone;
+    /**
+     * 用户性别
+     */
+    private Byte gender;
+    /**
+     * 用户身份证号
+     */
+    private String idCard;
+    /**
+     * 用户邮箱
+     */
+    private String email;
+    /**
+     * 用户年级
+     */
+    private Integer grade;
+    /**
+     * 用户专业
+     */
+    private String major;
+    /**
+     * 用户学号
+     */
+    private String studentId;
+    /**
+     * 用户实习/就职经历
+     */
+    private String experience;
+    /**
+     * 用户现状
+     */
+    private String currentStatus;
+    /**
+     * 用户加入时间
+     */
+    private String entryTime;
+    /**
+     * 用户所获得赞数量
+     */
+    private Integer likeCount;
+}
