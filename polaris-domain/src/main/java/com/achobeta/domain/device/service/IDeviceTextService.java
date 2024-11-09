ff7faea709909ba9a210ca23f57a41b096cb8b54
huangwenxing <2309377789@qq.com>
2024-11-09 22:37:00 +0800

    docs: 修改了设备service注释

--- polaris-domain/src/main/java/com/achobeta/domain/device/service/IDeviceTextService.java
+++ polaris-domain/src/main/java/com/achobeta/domain/device/service/IDeviceTextService.java
@@ -2,15 +2,14 @@ package com.achobeta.domain.device.service;
 
 import com.achobeta.domain.device.model.valobj.DeviceVO;
 
-import java.sql.SQLException;
 import java.util.List;
 
 /**
  * @author huangwenxing
- * @description 文本渲染服务统一接口
+ * @description 设备渲染服务统一接口
  * @date 2024/11/8
  */
 public interface IDeviceTextService {
     /**获取单页设备List*/
-    List<DeviceVO> getDeviceVO(String userId,String deviceId,int limit,String lastDeviceId) throws SQLException;
+    List<DeviceVO> getDeviceVO(String userId,String deviceId,int limit,String lastDeviceId);
 }
--- polaris-domain/src/main/java/com/achobeta/domain/device/service/get/GetDevicePostProcessor.java
+++ polaris-domain/src/main/java/com/achobeta/domain/device/service/get/GetDevicePostProcessor.java
@@ -5,7 +5,7 @@ import com.achobeta.types.support.postprocessor.BasePostProcessor;
 
 /**
  * @author huangwenxing
- * @description 设备渲染服务统一接口
+ * @description 设备渲染拓展统一接口
  * @date 2024/11/8
  */
 public interface GetDevicePostProcessor extends BasePostProcessor<DeviceBO> {
