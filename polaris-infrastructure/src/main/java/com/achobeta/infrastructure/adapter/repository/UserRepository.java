f877faa9561de7bf16cbd582d3fab46df94e8b19
Aseubel <1228392319@qq.com>
2024-11-10 12:30:54 +0800

    feat: 在repository添加日志打印

--- polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/UserRepository.java
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/UserRepository.java
@@ -29,19 +29,20 @@ public class UserRepository implements IUserRepository {
 
     @Override
     public UserEntity queryUserInfo(String userId) {
-        // 从redis中获取用户信息
+        log.info("尝试从redis中获取用户信息，userId: {}",userId);
         UserEntity userEntity = redisService.getValue(Constants.RedisKeyPrefix.USER_INFO + userId);
         if(userEntity!= null) {
             return userEntity;
         }
 
-        // 从数据库中获取用户信息
+        log.info("从数据库中查询用户信息，userId: {}",userId);
         UserPO userPO = userMapper.getUserByUserId(userId);
         if(userPO == null) {
             log.error("用户不存在！userId：{}",userId);
             throw new AppException(Constants.ResponseCode.USER_NOT_EXIST.getCode(),
                     Constants.ResponseCode.USER_NOT_EXIST.getInfo());
         }
+        log.info("从数据库中查询用户信息成功，userId: {}",userId);
         userEntity = UserEntity.builder()
                 .userId(userPO.getUserId())
                 .userName(userPO.getUserName())
@@ -58,19 +59,21 @@ public class UserRepository implements IUserRepository {
                 .likeCount(userPO.getLikeCount())
                 .build();
 
-        // 缓存用户信息到redis
+        log.info("将用户信息缓存到redis，userId: {}",userId);
         redisService.setValue(Constants.RedisKeyPrefix.USER_INFO + userId,userEntity);
+        log.info("将用户信息缓存到redis成功，userId: {}",userId);
         return userEntity;
     }
 
     @Override
     public void updateUserInfo(UserEntity userEntity) {
+        log.info("查询用户是否存在，userId: {}",userEntity.getUserId());
         if (userMapper.getUserByUserId(userEntity.getUserId()) == null) {
             log.error("用户不存在！userId：{}",userEntity.getUserId());
             throw new AppException(Constants.ResponseCode.USER_NOT_EXIST.getCode(),
                     Constants.ResponseCode.USER_NOT_EXIST.getInfo());
         }
-        // 更新用户信息到数据库
+        log.info("更新用户信息：{}",userEntity.getUserId());
          userMapper.updateUserInfo(UserPO.builder()
                  .userId(userEntity.getUserId())
                  .userName(userEntity.getUserName())
@@ -87,8 +90,11 @@ public class UserRepository implements IUserRepository {
                  .likeCount(userEntity.getLikeCount())
                  .updateBy(userEntity.getUserId())
                  .build());
-        // 更新用户信息到redis
+        log.info("更新用户信息成功，userId: {}",userEntity.getUserId());
+
+        log.info("更新用户信息缓存，userId: {}",userEntity.getUserId());
         redisService.setValue(Constants.RedisKeyPrefix.USER_INFO + userEntity.getUserId(),userEntity);
+        log.info("更新用户信息缓存成功，userId: {}",userEntity.getUserId());
     }
 
 }
