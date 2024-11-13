0657a2c45db4c09954c9293ad091efc19db9cb9f
Aseubel <1228392319@qq.com>
2024-11-13 19:39:41 +0800

    feat: 用户个人信息查看dao层编写

--- polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/UserRepository.java
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.achobeta.infrastructure.adapter.repository;
 
 import com.achobeta.domain.user.adapter.repository.IUserRepository;
+import com.achobeta.domain.user.model.entity.UserEntity;
 import com.achobeta.infrastructure.dao.UserMapper;
+import com.achobeta.infrastructure.dao.po.UserPO;
 import com.achobeta.infrastructure.redis.IRedisService;
+import com.achobeta.types.common.Constants;
+import com.achobeta.types.enums.GlobalServiceStatusCode;
+import com.achobeta.types.exception.AppException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
@@ -22,5 +27,43 @@ public class UserRepository implements IUserRepository {
 
     @Resource
     private IRedisService redisService;
+
+    @Override
+    public UserEntity queryUserInfo(String userId) {
+        log.info("尝试从redis中获取用户信息，userId: {}",userId);
+        UserEntity userEntity = redisService.getValue(Constants.USER_INFO + userId);
+        if(userEntity!= null) {
+            return userEntity;
+        }
+
+        log.info("从数据库中查询用户信息，userId: {}",userId);
+        UserPO userPO = userMapper.getUserByUserId(userId);
+        if(userPO == null) {
+            log.error("用户不存在！userId：{}",userId);
+            throw new AppException(String.valueOf(GlobalServiceStatusCode.USER_ACCOUNT_NOT_EXIST.getCode()),
+                    GlobalServiceStatusCode.USER_ACCOUNT_NOT_EXIST.getMessage());
+        }
+        log.info("从数据库中查询用户信息成功，userId: {}",userId);
+        userEntity = UserEntity.builder()
+                .userId(userPO.getUserId())
+                .userName(userPO.getUserName())
+                .phone(userPO.getPhone())
+                .gender(userPO.getGender())
+                .idCard(userPO.getIdCard())
+                .email(userPO.getEmail())
+                .grade(userPO.getGrade())
+                .major(userPO.getMajor())
+                .studentId(userPO.getStudentId())
+                .experience(userPO.getExperience())
+                .currentStatus(userPO.getCurrentStatus())
+                .entryTime(userPO.getEntryTime())
+                .likeCount(userPO.getLikeCount())
+                .build();
+
+        log.info("将用户信息缓存到redis，userId: {}",userId);
+        redisService.setValue(Constants.USER_INFO + userId,userEntity);
+        log.info("将用户信息缓存到redis成功，userId: {}",userId);
+        return userEntity;
+    }
 
 }
--- polaris-infrastructure/src/main/java/com/achobeta/infrastructure/dao/UserMapper.java
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.achobeta.infrastructure.dao;
 
+import com.achobeta.infrastructure.dao.po.UserPO;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -9,6 +10,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface UserMapper {
+    /**
+     * 根据userId获取用户信息
+     * @param userId 用户业务id
+     * @return 用户实体
+     */
+    UserPO getUserByUserId(String userId);
 
 
 }
