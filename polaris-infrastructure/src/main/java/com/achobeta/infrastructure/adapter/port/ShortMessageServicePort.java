61808bef673064d8aaa8e6d79ccb48a265420d57
hamster_yhz <1427739483@qq.com>
2024-11-13 22:41:26 +0800

    feat: 发送验证码的签名等字段移出到配置

--- polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/port/ShortMessageServicePort.java
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/port/ShortMessageServicePort.java
@@ -1,15 +1,10 @@
 package com.achobeta.infrastructure.adapter.port;
 
 import com.achobeta.domain.login.adapter.port.IShortMessageService;
-import com.github.qcloudsms.SmsSingleSender;
-import com.github.qcloudsms.SmsSingleSenderResult;
-import com.github.qcloudsms.httpclient.HTTPException;
-import org.json.JSONException;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import javax.annotation.Resource;
 import java.util.Random;
-import java.util.UUID;
 
 /**
  * @Author: 严豪哲
@@ -20,27 +15,22 @@ import java.util.UUID;
 
 @Component
 public class ShortMessageServicePort implements IShortMessageService {
+
+//    @Resource
+//    private ShortMessageServiceConfig sms;
+
     @Override
     public String sms(String phone) {
-        // 短信应用SDK AppID 短信应用的唯一标识，登录短信控制台后，在应用管理中查看
-        int appId=0;
-        // 短信应用SDK AppKey 密码
-        String appKey ="";
-        // 短信模板ID
-        int templateId=0;
-        // 签名
-        String smsSign="A";
+
         // 生成随机六位数字验证码
         Random random = new Random();
         String code = random.nextInt(900000) + 100000 + "";
-        //设置验证码的过期时间为5分钟
-        long expireTime = System.currentTimeMillis() + 5 * 60 * 1000;
         // 短信模板参数
-        String[] params={code,expireTime+""};
+        //String[] params={code,sms.getExpireTime()+""};
         // 发送短信
 //        try{
-//            SmsSingleSender ssender= new SmsSingleSender(appId,appKey);
-//            SmsSingleSenderResult result=ssender.sendWithParam("86",phone,templateId,params,smsSign,"","");
+//            SmsSingleSender ssender= new SmsSingleSender(sms.getAppId(),sms.getAppKey());
+//            SmsSingleSenderResult result=ssender.sendWithParam("86",phone,sms.getTemplateId(),params,sms.getSmsSign(),"","");
 //            System.out.println(result);
 //        } catch (HTTPException e) {
 //            throw new RuntimeException(e);
