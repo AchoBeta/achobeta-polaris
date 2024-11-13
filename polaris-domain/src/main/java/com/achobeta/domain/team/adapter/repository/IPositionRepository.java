03c33ad060b6f21feb20067f12ab6f15b2a57db2
Aseubel <1228392319@qq.com>
2024-11-14 00:03:59 +0800

    feat: 修改团队组织架构dao层编写

--- polaris-domain/src/main/java/com/achobeta/domain/team/adapter/repository/IPositionRepository.java
+++ polaris-domain/src/main/java/com/achobeta/domain/team/adapter/repository/IPositionRepository.java
@@ -11,5 +11,29 @@ import java.util.List;
  */
 public interface IPositionRepository {
 
+    /**
+     * 查询团队是否存在
+     * @author yangzhiyao
+     * @date 2024/11/11
+     * @param teamId 团队ID
+     * return Boolean 是否存在该团队
+     */
+    Boolean isTeamExists(String teamId);
+
+    /**
+     * 批量添加职位/分组
+     * @author yangzhiyao
+     * @date 2024/11/11
+     * @param positionEntityList
+     */
+    void savePosition(List<PositionEntity> positionEntityList, String teamId);
+
+    /**
+     * 批量删除职位/分组
+     * @author yangzhiyao
+     * @date 2024/11/11
+     * @param positionsToDelete
+     */
+    void deletePosition(List<PositionEntity> positionsToDelete, String teamId);
 
 }
--- polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/PositionRepository.java
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/PositionRepository.java
@@ -1,6 +1,5 @@
 package com.achobeta.infrastructure.adapter.repository;
 
-import cn.hutool.core.collection.CollectionUtil;
 import com.achobeta.domain.team.adapter.repository.IPositionRepository;
 import com.achobeta.domain.team.model.entity.PositionEntity;
 import com.achobeta.infrastructure.dao.PositionMapper;
@@ -23,6 +22,42 @@ import java.util.List;
 @Repository
 public class PositionRepository implements IPositionRepository {
 
+    @Resource
+    private PositionMapper positionMapper;
 
+    @Resource
+    private RedissonService redissonService;
 
+    @Override
+    public Boolean isTeamExists(String teamId) {
+        return positionMapper.getPositionByPositionId(teamId) != null;
+    }
+
+    @Override
+    public void savePosition(List<PositionEntity> positionEntityList, String teamId) {
+
+        // 封装到PositionPO对象中
+        List<PositionPO> positionPOList = new ArrayList<>();
+        for (PositionEntity positionEntity : positionEntityList) {
+            PositionPO positionPO = PositionPO.builder()
+                            .positionId(positionEntity.getPositionId())
+                            .positionName(positionEntity.getPositionName())
+                            .subordinate(positionEntity.getSubordinateId())
+                            .teamId(positionEntity.getTeamId())
+                            .level(positionEntity.getLevel())
+                            .build();
+            positionPOList.add(positionPO);
+        }
+
+        // 保存到数据库中
+        positionMapper.addPosition(positionPOList);
+
+        // 清除缓存
+        redissonService.remove(Constants.TEAM_STRUCTURE_SUBORDINATE + teamId + ":*");
+    }
+
+    @Override
+    public void deletePosition(List<PositionEntity> positionsToDelete, String teamId) {
+
+    }
 }
