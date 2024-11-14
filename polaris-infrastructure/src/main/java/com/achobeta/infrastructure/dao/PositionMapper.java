7c7607072a14072b4e513bc80cdf01dd1c9b3e94
Aseubel <1228392319@qq.com>
2024-11-14 20:34:49 +0800

    feat: 删掉多的mapper

--- polaris-infrastructure/src/main/java/com/achobeta/infrastructure/dao/PositionMapper.java
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/dao/PositionMapper.java
@@ -1,9 +1,6 @@
 package com.achobeta.infrastructure.dao;
 
-import com.achobeta.infrastructure.dao.po.PositionPO;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
 
 /**
  * @author yangzhiyao
@@ -12,50 +9,5 @@ import java.util.List;
  */
 @Mapper
 public interface PositionMapper {
-    /**
-     * 获取某个职位的所有下属职位
-     * @param positionId
-     * @return
-     */
-    List<PositionPO> listSubordinateByPositionId(String positionId);
 
-    /**
-     * 获取某个职位的信息
-     * @param positionId
-     * @return
-     */
-    PositionPO getPositionByPositionId(String positionId);
-
-    /**
-     * 获取某个职位的上级职位
-     * 用以查询用户的个人职位
-     * @param positionId
-     * @return
-     */
-    PositionPO getParentPositionByPositionId(String positionId);
-
-    /**
-     * 获取某个用户的所有最下级职位
-     * @param userId
-     * @return
-     */
-    List<PositionPO> listPositionByUserId(String userId);
-
-    /**
-     * 添加职位/分组
-     * @param positionPOList
-     */
-    void addPosition(List<PositionPO> positionPOList);
-
-    /**
-     * 在position表中删除职位/分组
-     * @param positionPOList
-     */
-    void deletePositionInPosition(List<PositionPO> positionPOList);
-
-    /**
-     * 在user_position关联表中删除职位/分组
-     * @param positionPOList
-     */
-    void deletePositionInUserPosition(List<PositionPO>positionPOList);
 }
