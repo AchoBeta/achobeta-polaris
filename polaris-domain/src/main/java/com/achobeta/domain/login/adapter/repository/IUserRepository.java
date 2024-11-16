2476a5e6a9d402b9cd0b01db89948efd7c569b31
hamster_yhz <1427739483@qq.com>
2024-11-16 20:23:08 +0800

    feat: 新增仓储层定义

---
+++ polaris-domain/src/main/java/com/achobeta/domain/login/adapter/repository/IUserRepository.java
@@ -0,0 +1,14 @@
+package com.achobeta.domain.login.adapter.repository;
+
+import com.achobeta.domain.login.model.entity.UserEntity;
+
+/**
+ * @Author: 严豪哲
+ * @Description: 新建设备 获取设备 查询用户 存储token
+ * @Date: 2024/11/10 16:19
+ * @Version: 1.0
+ */
+
+public interface IUserRepository {
+    UserEntity getUserByPhone(String phone);
+}
