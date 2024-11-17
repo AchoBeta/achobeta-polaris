7e8ea0bfca757428aed0d6237791df605fc7528d
Aseubel <1228392319@qq.com>
2024-11-18 00:50:45 +0800

    feat: 添加参数校验

--- polaris-api/src/main/java/com/achobeta/api/dto/user/ModifyUserInfoRequestDTO.java
+++ polaris-api/src/main/java/com/achobeta/api/dto/user/ModifyUserInfoRequestDTO.java
@@ -3,9 +3,7 @@ package com.achobeta.api.dto.user;
 import com.achobeta.types.annotation.FieldDesc;
 import lombok.*;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 /**
@@ -19,32 +17,35 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ModifyUserInfoRequestDTO implements Serializable {
-    /**
-     * 用户业务id
-     */
+
     @NotBlank(message = "用户业务id不能为空")
+    @FieldDesc(name = "用户业务id")
     private String userId;
 
     @FieldDesc(name = "用户名")
+    @Size(max = 20, message = "用户名长度不能超过20")
     private String userName;
 
     @FieldDesc(name = "性别")
     private Byte gender;
 
     @FieldDesc(name = "身份证号")
+    @Pattern(regexp = "^$|^(?:\\d{15}|\\d{17}(\\d|X))$", message = "身份证号格式不正确")
     private String idCard;
 
     @Email(message = "邮箱格式不正确")
     @FieldDesc(name = "邮箱")
     private String email;
 
+    @Min(value = 1970, message = "年级不合理")
+    @Max(value = 2050, message = "年级不合理")
     @FieldDesc(name = "年级")
     private Integer grade;
 
     @FieldDesc(name = "专业")
     private String major;
 
-    @Pattern(regexp = "^$|\\d{13}", message = "学号必须为空或为13位数字")
+    @Pattern(regexp = "^$|\\d{11,13}", message = "学号必须为空或为11到13位数字")
     @FieldDesc(name = "学号")
     private String studentId;
 
