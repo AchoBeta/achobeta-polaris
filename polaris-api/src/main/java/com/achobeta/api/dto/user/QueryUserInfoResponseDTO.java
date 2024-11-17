cc49d3402ddf941159010fc414fc3ac1e25240d9
Aseubel <1228392319@qq.com>
2024-11-18 00:55:02 +0800

    feat: 注解注释DTO

--- polaris-api/src/main/java/com/achobeta/api/dto/user/QueryUserInfoResponseDTO.java
+++ polaris-api/src/main/java/com/achobeta/api/dto/user/QueryUserInfoResponseDTO.java
@@ -1,5 +1,7 @@
 package com.achobeta.api.dto.user;
 
+import com.achobeta.types.annotation.FieldDesc;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.io.Serializable;
@@ -17,64 +19,50 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class QueryUserInfoResponseDTO implements Serializable {
-    /**
-     * 用户业务id
-     */
+
+    @FieldDesc(name = "用户ID")
     private String userId;
-    /**
-     * 用户名
-     */
+
+    @FieldDesc(name = "用户昵称")
     private String userName;
-    /**
-     * 用户手机号
-     */
+
+    @FieldDesc(name = "用户手机号")
     private String phone;
-    /**
-     * 用户性别
-     */
+
+    @FieldDesc(name = "用户性别")
     private Byte gender;
-    /**
-     * 用户身份证号
-     */
+
+    @FieldDesc(name = "身份证号")
     private String idCard;
-    /**
-     * 用户邮箱
-     */
+
+    @FieldDesc(name = "用户邮箱")
     private String email;
-    /**
-     * 用户年级
-     */
+
+    @FieldDesc(name = "年级")
     private Integer grade;
-    /**
-     * 用户专业
-     */
+
+    @FieldDesc(name = "专业")
     private String major;
-    /**
-     * 用户学号
-     */
+
+    @FieldDesc(name = "学号")
     private String studentId;
-    /**
-     * 用户实习/就职经历
-     */
+
+    @FieldDesc(name = "用户实习/就职经历")
     private String experience;
-    /**
-     * 用户现状
-     */
+
+    @FieldDesc(name = "用户现状")
     private String currentStatus;
-    /**
-     * 用户加入时间
-     */
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @FieldDesc(name = "用户加入时间")
     private LocalDateTime entryTime;
-    /**
-     * 用户所获得赞数量
-     */
+
+    @FieldDesc(name = "用户获赞数量")
     private Integer likeCount;
-    /**
-     * 点赞状态
-     */
+
+    @FieldDesc(name = "用户点赞状态")
     private Boolean liked;
-    /**
-     * 用户所属职位/分组
-     */
+
+    @FieldDesc(name = "用户职位信息")
     private List<List<String>> positions;
 }
