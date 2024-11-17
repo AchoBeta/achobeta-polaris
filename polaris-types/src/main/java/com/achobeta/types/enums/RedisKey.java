23fa0c2cfe13d875d0a86d46fbfc89609a377f25
hamster_yhz <1427739483@qq.com>
2024-11-17 14:35:58 +0800

    fix: 更改RdisKey

--- polaris-types/src/main/java/com/achobeta/types/enums/RedisKey.java
+++ polaris-types/src/main/java/com/achobeta/types/enums/RedisKey.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 /**
  * @Author: 严豪哲
- * @Description:
+ * @Description: redis的key前缀
  * @Date: 2024/11/10 23:26
  * @Version: 1.0
  */
@@ -14,10 +14,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public enum RedisKey {
-    CODE("ab:polaris:code:","验证码"),
-    RATE_LIMIT("ab:polaris:rateLimit:","发送验证码频率限制"),
-    TOKEN("ab:polaris:token:","token"),
-    LOGOUT("ab:polaris:logout:","设备id关联token"),
+    CODE("ab:polaris:login:code:","验证码"),
+    RATE_LIMIT("ab:polaris:login:rateLimit:","发送验证码频率限制"),
+    TOKEN("ab:polaris:login:token:","token"),
+    DEVICE_TO_TOKEN("ab:polaris:login:device_to_token:","设备id关联token"),
     ;
 
     private String keyPrefix;
