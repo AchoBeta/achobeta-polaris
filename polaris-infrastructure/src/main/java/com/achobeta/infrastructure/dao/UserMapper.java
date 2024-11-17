31e1e90913897da66cf0705b9fa58f128ff0fdac
Aseubel <1228392319@qq.com>
2024-11-17 14:46:23 +0800

    feat: 部分mapper及实体类创建

---
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/dao/UserMapper.java
@@ -0,0 +1,26 @@
+package com.achobeta.infrastructure.dao;
+
+import com.achobeta.infrastructure.dao.po.UserPO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author yangzhiyao
+ * @description UserDao接口
+ * @date 2024/11/5
+ */
+@Mapper
+public interface UserMapper {
+    /**
+     * 根据userId获取用户信息
+     * @param userId 用户业务id
+     * @return 用户实体
+     */
+    UserPO getUserByUserId(String userId);
+
+    /**
+     * 更新用户信息
+     * @param userPO 用户实体
+     */
+    void updateUserInfo(UserPO userPO);
+
+}
