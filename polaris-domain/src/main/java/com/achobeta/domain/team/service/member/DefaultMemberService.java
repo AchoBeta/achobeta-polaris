21c27332e9ddb97b5f0592dd5ebcbbd06859632c
Aseubel <1228392319@qq.com>
2024-11-17 23:00:56 +0800

    feat: 修正service层写法

--- polaris-domain/src/main/java/com/achobeta/domain/team/service/member/DefaultMemberService.java
+++ polaris-domain/src/main/java/com/achobeta/domain/team/service/member/DefaultMemberService.java
@@ -1,10 +1,11 @@
 package com.achobeta.domain.team.service.member;
 
+import com.achobeta.domain.team.adapter.repository.IMemberRepository;
 import com.achobeta.domain.team.model.bo.TeamBO;
 import com.achobeta.domain.team.service.IMemberService;
 import com.achobeta.domain.user.model.entity.UserEntity;
 import com.achobeta.types.enums.BizModule;
-import com.achobeta.types.support.postprocessor.AbstractPostProcessor;
+import com.achobeta.types.support.postprocessor.AbstractFunctionPostProcessor;
 import com.achobeta.types.support.postprocessor.PostContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,16 +21,33 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class DefaultMemberService extends AbstractPostProcessor<TeamBO> implements IMemberService {
+public class DefaultMemberService extends AbstractFunctionPostProcessor<TeamBO> implements IMemberService {
+
+    private final IMemberRepository memberRepository;
 
     @Override
     public UserEntity modifyMember(String teamId, UserEntity userEntity, List<String> addPositions, List<String> deletePositions) {
         PostContext<TeamBO> postContext = buildPostContext(teamId, userEntity);
         postContext.addExtraData("addPositions", addPositions);
         postContext.addExtraData("deletePositions", deletePositions);
-        postContext = super.doPostProcessor(postContext, ModifyMemberInfoPostProcessor.class);
-        ModifyMemberInfoPostProcessor processor = new ModifyMemberInfoPostProcessor();
-        postContext = processor.doMainProcessor(postContext);
+        postContext = super.doPostProcessor(postContext, ModifyMemberInfoPostProcessor.class,
+                new AbstractPostProcessorOperation<TeamBO>() {
+                    @Override
+                    public PostContext<TeamBO> doMainProcessor(PostContext<TeamBO> postContext) {
+                        TeamBO teamBO = postContext.getBizData();
+                        String teamId = teamBO.getTeamId();
+                        UserEntity userEntity = teamBO.getUserEntity();
+                        log.info("访问修改团队成员功能，开始处理，teamId: {}, userId: {}",teamId, userEntity.getUserId());
+
+                        memberRepository.modifyMemberInfo(userEntity, teamId,
+                                (List<String>)postContext.getExtraData("addPositions"),
+                                (List<String>)postContext.getExtraData("deletePositions"));
+
+                        log.info("访问修改团队成员功能，处理结束，teamId: {}, userId: {}",teamId, userEntity.getUserId());
+
+                        return postContext;
+                    }
+                });
         return postContext.getBizData().getUserEntity();
     }
 
@@ -39,10 +57,5 @@ public class DefaultMemberService extends AbstractPostProcessor<TeamBO> implemen
                 .bizId(BizModule.TEAM.getCode())
                 .bizData(TeamBO.builder().teamId(teamId).userEntity(userEntity).build())
                 .build();
-    }
-
-    @Override
-    public PostContext<TeamBO> doMainProcessor(PostContext<TeamBO> postContext) {
-        return postContext;
     }
 }
--- polaris-domain/src/main/java/com/achobeta/domain/team/service/member/ModifyMemberInfoPostProcessor.java
+++ polaris-domain/src/main/java/com/achobeta/domain/team/service/member/ModifyMemberInfoPostProcessor.java
@@ -1,15 +1,7 @@
 package com.achobeta.domain.team.service.member;
 
-import com.achobeta.domain.team.adapter.repository.IMemberRepository;
-import com.achobeta.domain.team.model.bo.TeamBO;
-import com.achobeta.domain.user.model.entity.UserEntity;
-import com.achobeta.types.support.postprocessor.AbstractPostProcessor;
-import com.achobeta.types.support.postprocessor.PostContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author yangzhiyao
@@ -18,24 +10,6 @@ import java.util.List;
  */
 @Slf4j
 @Component
-public class ModifyMemberInfoPostProcessor extends AbstractPostProcessor<TeamBO> implements MemberPostProcessor {
+public class ModifyMemberInfoPostProcessor implements MemberPostProcessor {
 
-    @Resource
-    private IMemberRepository memberRepository;
-
-    @Override
-    public PostContext<TeamBO> doMainProcessor(PostContext<TeamBO> postContext) {
-        TeamBO teamBO = postContext.getBizData();
-        String teamId = teamBO.getTeamId();
-        UserEntity userEntity = teamBO.getUserEntity();
-        log.info("访问修改团队成员功能，开始处理，teamId: {}, userId: {}",teamId, userEntity.getUserId());
-
-        memberRepository.modifyMemberInfo(userEntity, teamId,
-                (List<String>)postContext.getExtraData("addPositions"),
-                (List<String>)postContext.getExtraData("deletePositions"));
-
-        log.info("访问修改团队成员功能，处理结束，teamId: {}, userId: {}",teamId, userEntity.getUserId());
-
-        return postContext;
-    }
 }
