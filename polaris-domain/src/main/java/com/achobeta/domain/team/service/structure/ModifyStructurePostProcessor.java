7c900145abf992a34e772268a3bbb799b147fdd7
Aseubel <1228392319@qq.com>
2024-11-22 18:31:58 +0800

    feat: 修改PostProcessor结构

--- polaris-domain/src/main/java/com/achobeta/domain/team/service/structure/ModifyStructurePostProcessor.java
+++ polaris-domain/src/main/java/com/achobeta/domain/team/service/structure/ModifyStructurePostProcessor.java
@@ -1,11 +1,9 @@
 package com.achobeta.domain.team.service.structure;
 
-import com.achobeta.types.support.postprocessor.BasePostProcessor;
-
 /**
  * @author yangzhiyao
  * @description 修改团队组织架构扩展接口
  * @date 2024/11/11
  */
-public interface ModifyStructurePostProcessor extends BasePostProcessor {
+public interface ModifyStructurePostProcessor extends StructurePostProcessor{
 }
