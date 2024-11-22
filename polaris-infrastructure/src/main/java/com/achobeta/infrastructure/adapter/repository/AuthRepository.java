99fc96de497976bbf8ec6f5a877e98f70702f4a3
Aseubel <1228392319@qq.com>
2024-11-22 22:18:21 +0800

    feat: 添加Redis缓存

--- polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/AuthRepository.java
+++ polaris-infrastructure/src/main/java/com/achobeta/infrastructure/adapter/repository/AuthRepository.java
@@ -1,11 +1,14 @@
 package com.achobeta.infrastructure.adapter.repository;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.achobeta.domain.auth.adapter.repository.IAuthRepository;
 import com.achobeta.domain.auth.model.entity.RoleEntity;
 import com.achobeta.infrastructure.dao.PermissionMapper;
 import com.achobeta.infrastructure.dao.RoleMapper;
 import com.achobeta.infrastructure.dao.po.PermissionPO;
 import com.achobeta.infrastructure.dao.po.RolePO;
+import com.achobeta.infrastructure.redis.IRedisService;
+import com.achobeta.types.common.RedisKey;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
@@ -28,10 +31,18 @@ public class AuthRepository implements IAuthRepository {
     @Resource
     private PermissionMapper permissionMapper;
 
+    @Resource
+    private IRedisService redisService;
+
     @Override
     public List<RoleEntity> queryRoles(String userId, String teamId) {
+        List<RoleEntity> roleEntityList = redisService.getValue(RedisKey.USER_ROLE + userId);
+        if (!CollectionUtil.isEmpty(roleEntityList)) {
+            return roleEntityList;
+        }
+
         List<RolePO> rolePOList = roleMapper.listRole(userId, teamId);
-        List<RoleEntity> roleEntityList = new ArrayList<>(rolePOList.size());
+        roleEntityList = new ArrayList<>(rolePOList.size());
 
         for (RolePO rolePO : rolePOList) {
             roleEntityList.add(RoleEntity.builder()
@@ -42,21 +53,28 @@ public class AuthRepository implements IAuthRepository {
                     .build());
         }
 
+        redisService.setValue(RedisKey.USER_ROLE + userId, roleEntityList);
         return roleEntityList;
     }
 
     @Override
     public List<String> queryPermissions(String userId, List<String> roleIds) {
+        List<String> permissionNames = redisService.getValue(RedisKey.USER_PERMISSION + userId);
+        if (!CollectionUtil.isEmpty(permissionNames)) {
+            return permissionNames;
+        }
+
         if (roleIds == null || roleIds.isEmpty()) {
             return Collections.emptyList();
         }
 
         List<PermissionPO> permissions = permissionMapper.listPermission(userId, roleIds);
-        List<String> permissionNames = new ArrayList<>(permissions.size());
+        permissionNames = new ArrayList<>(permissions.size());
         for (PermissionPO permission : permissions) {
             permissionNames.add(permission.getPermissionName());
         }
 
+        redisService.setValue(RedisKey.USER_PERMISSION + userId, permissionNames);
         return permissionNames;
     }
 }
---
+++ polaris-types/src/main/java/com/achobeta/types/common/RedisKey.java
@@ -0,0 +1,8 @@
+package com.achobeta.types.common;
+
+public class RedisKey {
+
+    public static final String USER_PERMISSION = "ab:polaris:user:permission:";
+
+    public static final String USER_ROLE = "ab:polaris:user:permission:";
+}
