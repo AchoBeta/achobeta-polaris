4caf1bace5c58f4989deec7fb624174f233cd409
Aseubel <1228392319@qq.com>
2024-11-22 14:43:20 +0800

    feat: 切口注解编写

--- polaris-app/src/main/java/com/achobeta/aop/AuthVerifyAspect.java
+++ polaris-app/src/main/java/com/achobeta/aop/AuthVerifyAspect.java
@@ -3,17 +3,20 @@ package com.achobeta.aop;
 import com.achobeta.domain.auth.adapter.repository.IAuthRepository;
 import com.achobeta.domain.auth.model.entity.RoleEntity;
 import com.achobeta.types.annotation.AuthVerify;
+import com.achobeta.types.exception.AppException;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.achobeta.types.enums.GlobalServiceStatusCode.USER_NO_PERMISSION;
 
 /**
  * @author yangzhiyao
@@ -28,27 +31,22 @@ public class AuthVerifyAspect {
     @Resource
     private IAuthRepository authRepository;
 
-    @Pointcut("annotation(AuthVerify")
-    public void pointCut() {}
-
     /**
      * 鉴权处理
      * @param point 信息
-     * @param authVerify 鉴权注解
      * @return Object
      * @throws Throwable 异常
      */
-    @Around("pointCut()")
-    public Object verify(ProceedingJoinPoint point, AuthVerify authVerify) throws Throwable {
+    @Before("@annotation(com.achobeta.types.annotation.AuthVerify)")
+    public void verify(JoinPoint point) throws Throwable {
         Object arg = point.getArgs()[0];
-        Method method = arg.getClass().getMethod("getUserId");
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
         // 获取用户ID，团队ID
-        String userId = (String) method.invoke(arg);
-        String teamId = (String) method.invoke(arg);
+        String userId = (String) arg.getClass().getMethod("getUserId").invoke(arg);
+        String teamId = (String) arg.getClass().getMethod("getTeamId").invoke(arg);
         // 获取需要的角色和权限
-        String[] needed = method.getAnnotation(AuthVerify.class).value();
-        String[] neededRoles = needed[0].split(":");
-        String[] neededPermissions = needed[1].split(":");
+        String[] needed = method.getAnnotation(AuthVerify.class).value().split(":");
         // 查询用户角色和权限
         List<RoleEntity> userRoles = authRepository.queryRoles(userId, teamId);
         List<String> userRoleIds = new ArrayList<>();
@@ -56,9 +54,15 @@ public class AuthVerifyAspect {
             userRoleIds.add(userRole.getRoleId());
         }
         List<String> userPermissions = authRepository.queryPermissions(userId, userRoleIds);
-        if ()
-
-        return point.proceed();
+        // 判断用户是否有权限
+        for (String neededPermission : needed) {
+            if (userPermissions.contains(neededPermission)) {
+                return;
+            }
+        }
+        log.error("用户 {} 无权限访问方法 {} ", userId, point.getSignature().getName());
+        throw new AppException(USER_NO_PERMISSION.getCode().toString(),
+                USER_NO_PERMISSION.getMessage());
     }
 
 }
--- polaris-types/src/main/java/com/achobeta/types/annotation/AuthVerify.java
+++ polaris-types/src/main/java/com/achobeta/types/annotation/AuthVerify.java
@@ -1,9 +1,6 @@
 package com.achobeta.types.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author yangzhiyao
@@ -13,5 +10,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AuthVerify {
-    String[] value() default {};
+    String value() default "";
 }
