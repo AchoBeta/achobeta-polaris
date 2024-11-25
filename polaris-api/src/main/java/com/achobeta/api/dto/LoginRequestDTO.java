8974a4a96a8d1e36a68c758b2745d76b35daa8f5
hamster_yhz <1427739483@qq.com>
2024-11-25 10:33:06 +0800

    fix: 更改为Fingerprinting控制层

--- polaris-api/src/main/java/com/achobeta/api/dto/LoginRequestDTO.java
+++ polaris-api/src/main/java/com/achobeta/api/dto/LoginRequestDTO.java
@@ -26,7 +26,6 @@ public class LoginRequestDTO {
      * 登录用户的手机号
      */
     @Pattern(regexp = "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$", message = "请检查手机号是否正确")
-    @NotBlank(message = "请检查手机号是否正确")
     private String phone;
 
     /*
@@ -39,13 +38,11 @@ public class LoginRequestDTO {
      * 登录用户的ip
      */
     @Pattern(regexp = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", message = "ip格式错误")
-    @NotBlank(message = "ip不能为空")
     private String ip;
 
     /*
      * 登录用户的是否自动登录
      */
-    @NotBlank(message = "是否自动登录不能为空")
     @Pattern(regexp = "^(true|false)$", message = "是否自动登录只能是true或者false")
     private String isAutoLogin;
 
@@ -55,10 +52,9 @@ public class LoginRequestDTO {
     private boolean autoLogin;
 
     /*
-     * 登录用户的mac地址
+     * 登录用户的设备指纹
      */
-    @Pattern(regexp = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", message = "mac地址格式错误")
-    @NotBlank(message = "mac不能为空")
-    private String mac;
+    @Pattern(regexp = "", message = "设备指纹格式错误")
+    private String fingerprinting;
 
 }
