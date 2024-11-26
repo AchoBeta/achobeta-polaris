0e376f2ccac2ef7fdf3c5373442a1fecc94b7938
Aseubel <1228392319@qq.com>
2024-11-26 15:11:09 +0800

    feat: 调整添加成员返回已存在成员信息的字段顺序，补充注解

--- polaris-api/src/main/java/com/achobeta/api/dto/AddMemberResponseDTO.java
+++ polaris-api/src/main/java/com/achobeta/api/dto/AddMemberResponseDTO.java
@@ -15,10 +15,9 @@ import lombok.*;
 @Builder
 public class AddMemberResponseDTO {
 
-    @FieldDesc(name = "已存在的用户信息")
-    Object userInfo;
-
     @FieldDesc(name = "状态码,0表示添加成功，1表示用户已存在")
     Integer statusCode;
 
+    @FieldDesc(name = "已存在的用户信息")
+    Object userInfo;
 }
